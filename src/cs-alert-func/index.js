32552b0cd0173a711c77b5524edd02ad7674e15d
ASHOK PV <pvashok854@gmail.com>
2019-05-23 14:45:41 +0530

    Updated index js for schedule stop url

--- src/cs-alert-func/index.js
+++ src/cs-alert-func/index.js
@@ -1,7 +1,7 @@
 module.exports = function (context, req) {
     let query = req.query ? req.query : null;
     let req_body = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
-    let error = !req_body || !query ? "invalid payload in body" : null;
+    let error = !query ? "invalid query" : null;
 
     if (error) {
         context.res = {
@@ -17,7 +17,15 @@ module.exports = function (context, req) {
 
         let queue_message = { "body-json": req_body }
         if (query) {
+            if (query.schedule_id){
+                queue_message.service_acc_id = query.service_account_id;
+                queue_message.project_id = query.project_id;
+                queue_message.schedule_id = query.schedule_id;
+                queue_message.alert_type = "stop_schedule_alert";
+            }
+            else{
             queue_message.query_params = query;
+            }
         }
         context.bindings.out = queue_message;
     }
