2ccca9b26e53746ac66db1b9780d458b55594d51
ashok <ashok@corestack.io>
2019-07-10 12:55:31 +0530

    Added Snow func

--- src/snow-func/index.js
+++ src/snow-func/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, req) {
     let query = req.query ? req.query : null;
     let req_body = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
     let error = !query ? "invalid query" : null;
-    context.log("started func");
+
     if (error) {
         context.res = {
             status: 500,
@@ -15,20 +15,11 @@ module.exports = function (context, req) {
             body: { "message": "Corestack is processing the message. Thanks for using corestack." }
         };
 
+        let queue_message = req_body;
         if (query) {
-            if (query.queue_name){
-                if (query.queue_name == 'activity_log'){
-                    context.bindings.output = req_body;
-                }
-                else if (query.queue_name == 'shallow_discovery'){
-                    context.log(req_body)
-                    context.bindings.out = req_body;
-                }
-                
-            }
-            
+            queue_message.query_params = query;
         }
-        
+        context.bindings.out = queue_message;
     }
     context.done();
 };
